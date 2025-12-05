26151b2b9698c5d897db9f5b0c41dfb112d0a9ca
Yash Arya <yasharya2901@gmail.com>
2025-12-05 15:58:32 +0530

    feat: rename database connection function and enhance Blog and Tag schemas for improved structure and type safety

--- src/server/db/models/blog.ts
+++ src/server/db/models/blog.ts
@@ -2,8 +2,12 @@ import { model, models, Schema, type Document, type Model } from "mongoose";
 import type { BlogBase } from "../../types/Blog";
 
 
-export type BlogDocument = BlogBase & 
-    Document & {
+export type BlogDocument = Omit<BlogBase, "authorId" | "tagIds" | "datePublished"> & 
+    {
+        authorId: Schema.Types.ObjectId;
+        tagIds: Schema.Types.ObjectId[];
+        datePublished: Date | null;
+    } & Document & {
         createdAt: Date;
         updatedAt: Date;
         deletedAt: Date | null;
@@ -13,12 +17,12 @@ const BlogSchema = new Schema<BlogDocument>(
     {
         title: {
             type: String,
-            required: true,
+            required: false,
             trim: true,
         },
         slug: {
             type: String,
-            required: true,
+            required: false,
             unique: true,
             lowercase: true,
             trim: true,
@@ -26,22 +30,25 @@ const BlogSchema = new Schema<BlogDocument>(
         },
         content: {
             type: String,
-            required: true,
+            required: false,
         },
         authorId: {
-            type: String,
+            type: Schema.Types.ObjectId,
+            ref: "User",
             required: true,
-            trim: true,
+            index: true,
         },
         datePublished: {
             type: Date,
             required: false
         },
-        tagIds: {
-            type: [String],
-            required: true,
-            default: []
-        },
+        tagIds: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Tag",
+                index: true,
+            }
+        ],
         deletedAt: {
             type: Date,
             required: false,
